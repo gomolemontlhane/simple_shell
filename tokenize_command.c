9ca6df58ea288409e9beaa09d8e925338c9d52c3
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-15 15:51:24 -0800

    updated tokenize_command.c

--- tokenize_command.c
+++ tokenize_command.c
@@ -6,11 +6,14 @@
  */
 void tokenize_command(char *command)
 {
-	char *token = strtok(command, " \n");
+	char *token;
+	char *delimiters = " \t\n";	/* Space, tab, and newline as delimiters */
+
+	token = strtok(command, delimiters);
 
 	while (token != NULL)
 	{
-		my_printf("%s\n", token);
-		token = strtok(NULL, " \n");
+		my_printf("Token: %s\n", token);
+		token = strtok(NULL, delimiters);
 	}
 }
