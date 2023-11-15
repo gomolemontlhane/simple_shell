f04f7c40e16b831b80e2b0bf1ae2f0ef30e0fb9f
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-15 13:26:37 -0800

    updated header.h and shell.c

--- header.h
+++ header.h
@@ -8,12 +8,15 @@
 #include <sys/wait.h>
 #include <string.h>
 
+/* Include this line to declare environ */
+extern char **environ;
+
 /* Function prototypes */
 char *readline(void);
 char **split_input(char *input);
 int execute_command(char **args);
 int _strlen(char *str);
-char *find_path(char *command);
+char *find_path(void);  /* Update the declaration here */
 int is_exit(char *command);
 int is_env(char *command);
 
