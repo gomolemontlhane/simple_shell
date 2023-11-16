dc1327c0c7bcec96323cb30109a9b8510ec8112d
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-15 17:37:10 -0800

    added interactive and non-interactive mode main program

--- simple_shell.c
+++ simple_shell.c
@@ -1,24 +1,38 @@
 #include "shell.h"
+
 /**
- * main - Entry point for the simple shell program
+ *main - Entry point for the simple shell program
+ *@argc: The number of command-line arguments
+ *@argv: An array of strings containing the command-line arguments
  *
- * Return: Always 0
+ *Return: Always 0
  */
-int main(void)
+int main(int argc, char **argv)
 {
 	char *command;
 
-	/* Loop for interactive mode */
-	while (1)
+	if (argc > 1) /*Check if command-line arguments are provided */
 	{
-		display_prompt();
-		command = read_command();
-		tokenize_command(command);
-		execute_command(command);
+		command = read_command_from_file(argv[1]); /*Read command from file */
+		execute_command(command); /*Execute the command */
+		free(command); /*Free the allocated memory for the command */
+	}
+	else
+	{
+		/*Interactive mode */
+		while (1)
+		{
+			display_prompt(); /*Display the prompt */
+			command = read_command(); /*Read user input */
 
-		/* Free allocated memory for command */
-		free(command);
+			if (!command)
+				break; /*Exit the loop if Ctrl+D is pressed */
+
+			tokenize_command(command); /*Tokenize the command */
+			execute_command(command); /*Execute the command */
+			free(command); /*Free the allocated memory for the command */
+		}
 	}
 
-	return (0);
+	return 0;
 }
